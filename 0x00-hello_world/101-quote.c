ddbd45c9c91788b6899e642470dd9eb4995d9c5e
ZIN-EDDIN <zineddin702@gmail.com>
2023-03-20 18:33:26 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+*main - A C program that prints a line given in task 8 of project hello world
+*Return: Always 1 (Success)
+*/
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return	(1);
+}
+
